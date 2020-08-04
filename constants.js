680dcd47bd6a09189ccfe872b2426f3f2330fb98
hhy <haoyuanhe@gmail.com>
2020-08-04 16:30:13 +0800

    add codes

--- constants.js
+++ constants.js
@@ -1,13 +1,13 @@
 var Constants = Constants || {
     ffmpeg: "/home/tengits/ffmpeg/ffmpeg",
     imageInterval: 5,
-    imagePath: "/Users/apple/nms/img/",
+    imagePath: "/home/qcjk/nms/img/",
     nginxPath: "/www/snapshot/",
     COMMON_SPLIT_STR: "|",
     snapShotTime: 120,          // 定时上传截图到cos时间间隔120s
     TimeTick: 15,               // 系统tick时间15s
-    imageURL: "http://172.20.91.211:13050/",
-    cloudURL: "http://172.20.92.168:13026",
+    imageURL: "http://172.20.91.23:13050/",
+    cloudURL: "http://172.20.91.23:13026",
     serverId: 1,
     systemId: 8,
     hd_str: "_hd",
